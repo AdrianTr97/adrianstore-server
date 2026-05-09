c948cfda7513a22d198fd5ad9438243f64e7636a
Adrian Trajano <45612732+AdrianTr97@users.noreply.github.com>
2026-05-08 21:26:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const rotaLivro = require("./routes/livros");
+//favorito
+const rotaFavorito = require("./routes/favoritos");
 
 const app = express();
 const port = 8000;
@@ -7,9 +9,9 @@ const port = 8000;
 app.use(express.json());
 
 app.use('/livros', rotaLivro);
+//rota do favorito
+app.use('/favoritos', rotaFavorito);
 
 app.listen(port, () => {
     console.log(`Escutando a porta ${port}`)
 })
-
-
